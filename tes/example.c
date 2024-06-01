4982c9130ba020efd87a52ff319fb5a2161a3bc2
hewo233 <2842137843@qq.com>
2024-06-01 10:36:41 +0800

    link test

--- tes/example.c
+++ tes/example.c
@@ -1,12 +1,20 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h> // Include the necessary header file
 
-int global_variable = 42;
+int main()
+{
+    pid_t pid;
 
-void my_function() {
-    printf("Hello, world!\n");
-}
-
-int main() {
-    my_function();
-    return 0;
+    int x = 1;
+    pid = fork();
+    printf("pid is : %d",pid);
+    if(pid == 0)
+    {
+        printf("Child: x = %d\n", ++x);
+        exit(0);
+    }
+    printf("Parent: x = %d\n", --x);
+    exit(0);
 }
