8572bfeda3db2fa8ab29883a490087abb85c835b
hewo233 <2842137843@qq.com>
2024-06-15 14:02:51 +0800

    main

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -1,14 +1,76 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+#define MAXLINE 8192
+
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
+void SIGPIPE_handler(int sig)
 {
-    printf("%s", user_agent_hdr);
+    fprintf(stderr, "SIGPIPE caught\n");
+    return;
+}
+
+struct URI
+{
+    char hostname[MAXLINE];
+    char port[MAXLINE];
+    char path[MAXLINE];
+};
+
+void doit(int connfd)
+{
+    rio_t rio, server_rio;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    
+    Rio_readinitb(&rio, connfd);
+    Rio_readlineb(&rio, buf, MAXLINE);
+    sscanf(buf, "%s %s %s", method, uri, version);
+
+    if(strcasecmp(method, "GET"))
+    {
+        fprintf(stderr, "GET is not allowed\n");
+        return;
+    }
+
+
+}
+
+int main(int argc, char **argv) 
+{
+    if(argc != 2)
+    {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    int listenfd, connfd;
+    socklen_t clientlen;
+    char hostname[MAXLINE], port[MAXLINE];
+    struct sockaddr_storage clientaddr;
+
+    signal(SIGPIPE, SIGPIPE_handler);
+
+    listenfd = Open_listenfd(argv[1]);
+    while(1)
+    {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+
+        doit(connfd);
+
+        Close(connfd);
+    }
+
     return 0;
 }
