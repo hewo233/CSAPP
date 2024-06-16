cb6370f18a609b3fa95c0524f57a32c366984862
hewo <2842137843@qq.com>
2024-06-16 22:22:43 +0800

    sbuf mode

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -153,6 +153,52 @@ void doit(int connfd)
     close(serverfd);
 }
 
+typedef struct
+{
+    int *buf;
+    int n;
+    int front;
+    int rear;
+    sem_t mutex;
+    sem_t slots;
+    sem_t items;
+} sbuf_t;
+
+void sbuf_init(sbuf_t *sp, int n)
+{
+    sp->buf = Calloc(n, sizeof(int));
+    sp->n = n;
+    sp->front = sp->rear = 0;
+    Sem_init(&sp->mutex, 0, 1);
+    Sem_init(&sp->slots, 0, n);
+    Sem_init(&sp->items, 0, 0);
+}
+
+void sbuf_deinit(sbuf_t *sp)
+{
+    Free(sp->buf);
+}
+
+void sbuf_insert(sbuf_t *sp, int item)
+{
+    P(&sp->slots);
+    P(&sp->mutex);
+    sp->buf[(++sp->rear)%(sp->n)] = item;
+    V(&sp->mutex);
+    V(&sp->items);
+}
+
+int sbuf_remove(sbuf_t *sp)
+{
+    int item;
+    P(&sp->items);
+    P(&sp->mutex);
+    item = sp->buf[(++sp->front)%(sp->n)];
+    V(&sp->mutex);
+    V(&sp->slots);
+    return item;
+}
+
 int main(int argc, char **argv) 
 {
     if(argc != 2)
@@ -169,11 +215,8 @@ int main(int argc, char **argv)
     signal(SIGPIPE, SIGPIPE_handler);
 
     listenfd = Open_listenfd(argv[1]);
-    if(listenfd < 0)
-    {
-        fprintf(stderr, "Open_listenfd error\n");
-        exit(1);
-    }
+
+
     while(1)
     {
         clientlen = sizeof(clientaddr);
